17c0146308e8f7d5746e83bddd3bc8246eb5577c
Janie Tito <jtito@hotmail.com>
2017-05-27 13:44:31 +1200

    main styling

--- index.js
+++ index.js
@@ -7,10 +7,21 @@ const _ = require('lodash')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 app.get('/', function (req, res, next) {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
+
+app.get('/business',function(req,res){
+  res.sendFile(path.join(__dirname, 'businessForm.html'))
+});
+
+app.get('/community',function(req,res){
+  res.sendFile(path.join(__dirname, 'communityForm.html'))
+});
+
 
 app.post('/submit', function(req, res, next){
   res.send("hello")
